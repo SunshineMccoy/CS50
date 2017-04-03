9bd5a2392649bd4cb0fb04872e07da1f4fd8d385
SunshineMccoy <mccoymichaeljohn@gmail.com>
2017-04-03 10:46:16 -0400

    Create initials1.c

---
+++ pset2/initials1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main (void)
+{
+    
+    string s = get_string();
+    printf("%c", s[0]);
+    int n = strlen(s);
+    for (int i = 0; i < n; i++ ) {
+        if (s[i] == ' ' && s[i+1] != ' ') {
+            printf("%c", s[i+1]);
+        }
+    }
+    printf("\n");
+    
+}
